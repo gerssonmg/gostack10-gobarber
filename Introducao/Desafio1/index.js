6ca78013da5c9f1fc10548bdd3fceb7a1cb211bb
Gerson <gersoncafilho@gmail.com>
2020-01-28 10:33:16 -0300

    Concluded logic

--- Introducao/Desafio1/index.js
+++ Introducao/Desafio1/index.js
@@ -6,40 +6,78 @@ server.use(express.json());
 
 const projectsBD = [];
 
-server.get('/projects', (req, res) => {
-  return res.send(projectsBD);
-});
+/**
+ * Middlewares
+ */
+function projectFind(req, res, next) {
+  if (!projectsBD.find(project => project.id === req.params.id)) {
 
-server.post('/projects', (req, res) => {
-  const { id, title, tasks } = req.body;
-  projectsBD.push({ id, title, tasks });
-  return res.send(projectsBD);
-});
+    console.log('AQUI');
+    return res.status(400).json({ error: 'Project not found' });
+  }
+  next();
+}
+
+function printLogCount(req, res, next) {
+  console.count('Numero de requisicoes');
+  next();
+}
 
 /**
- * 
+ * Routes
  */
-server.put('/projects/:id', (req, res) => {
+server.get('/projects', printLogCount, (req, res) => {
+  return res.json(projectsBD);
+});
+
+server.get('/projects/:id', printLogCount, projectFind, (req, res) => {
+  console.log(req.params.id);
+  return res.json(projectsBD.find(project => project.id === req.params.id));
+});
+
+server.post('/projects', printLogCount, (req, res) => {
+  const { id, title, tasks } = req.body;
+  projectsBD.push({ id, title, tasks });
+  return res.json(projectsBD);
+});
+
+server.post('/projects/:id/tasks', printLogCount, projectFind, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
-  console.time('Put');
-  const project_att = projectsBD.map(project => {
+  const project = projectsBD.find(project => project.id === id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+})
+
+server.put('/projects/:id', printLogCount, projectFind, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  projectsBD.map(project => {
     if (project.id === id) {
       project.title = title;
     }
   });
-  console.timeEnd('Put');
 
-  console.log('/n');
+  return res.json(projectsBD);
+})
 
-  console.time('Put');
-  const project = projectsBD.find(p => p.id === id);
+server.delete('/projects/:id', printLogCount, projectFind, (req, res) => {
 
-  project.title = title + 'rocketseat';
-  console.timeEnd('Put');
+  const { id } = req.params;
+
+  const projectIndex = projectsBD.findIndex(project => project.id === id);
+
+  if (projectIndex !== -1) {
+    projectsBD.splice(projectIndex, 1);
+  }
 
   return res.send(projectsBD);
 })
+
+
 
 server.listen(3000);
