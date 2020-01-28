d8166575203aaa4e6a4cdc0849db3c7146f0ad17
Gerson <gersoncafilho@gmail.com>
2020-01-28 00:17:26 -0300

    Finish nodeJS introduction

--- Introducao/NodeJs/index.js
+++ Introducao/NodeJs/index.js
@@ -12,25 +12,27 @@ const users = ['Diego', 'Claudio', 'Victor'];
 
 server.use((req, res, next) => {
 
-  return next();
+  console.time('Req');
+  next();
+  console.timeEnd('Req');
 });
 
 server.get('/users', (req, res) => {
   return res.json(users);
 });
 
-server.get('/users/:index', (req, res) => {
-  const { index } = req.params;
-  return res.send({ "u": users[index] });
+server.get('/users/:index', checkUserInArray, (req, res) => {
+  return res.json(req.use);
 });
 
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExists, (req, res) => {
   const { name } = req.body;
+  console.log(name);
   users.push(name);
   return res.json(users);
 })
 
-server.put('users/:index', (req, res) => {
+server.put('users/:index', checkUserInArray, checkUserExists, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -39,9 +41,27 @@ server.put('users/:index', (req, res) => {
   return res.json(users);
 })
 
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index', checkUserInArray, (req, res) => {
   const { index } = req.params;
   users.splice(index, 1);
   return res.send();
 })
+
+function checkUserInArray(req, res, next) {
+  const use = users[req.params.index];
+
+  if (!use) {
+    return res.status(400).json({ error: 'User not find is required' });
+  }
+  req.use = use;
+  return next();
+}
+
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'User name is required' });
+  }
+  return next();
+}
+
 server.listen(3000);
