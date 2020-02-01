29b0ddea44f9cf2ebbaffb715257606b64fe1d13
Gerson <gersoncafilho@gmail.com>
2020-02-01 20:10:05 -0300

    Validation addpointment date old/ date not available

--- GoBarbera/GoBarber-Backend/src/app/controllers/ProviderController.js
+++ GoBarbera/GoBarber-Backend/src/app/controllers/ProviderController.js
@@ -1,5 +1,5 @@
 import User from '../models/User';
-import File from '../models/Files';
+import File from '../models/File';
 
 class ProviderController {
   async index(req, res) {
--- GoBarbera/GoBarber-Backend/src/routes.js
+++ GoBarbera/GoBarber-Backend/src/routes.js
@@ -6,6 +6,7 @@ import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import AuthMiddleware from './app/middlewares/auth';
 
@@ -19,6 +20,9 @@ routes.use(AuthMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
