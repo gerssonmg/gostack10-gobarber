4d3ea56adcf50a93b1a0e0f5ff47aea760afd319
Gerson <gersoncafilho@gmail.com>
2020-02-01 09:32:31 -0300

    view files

--- GoBarbera/GoBarber-Backend/src/app/controllers/UserController.js
+++ GoBarbera/GoBarber-Backend/src/app/controllers/UserController.js
@@ -68,13 +68,17 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
-    return res.json({
-      id,
-      name,
-      email,
-      provider,
-    });
+    try {
+      const { id, name, provider } = await user.update(req.body);
+      return res.json({
+        id,
+        name,
+        email,
+        provider,
+      });
+    } catch (err) {
+      return res.status(400).json({ error: `Error update: ${err.name}` });
+    }
   }
 }
 
--- GoBarbera/GoBarber-Backend/src/routes.js
+++ GoBarbera/GoBarber-Backend/src/routes.js
@@ -1,15 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
+import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import ProviderController from './app/controllers/ProviderController';
 
 import AuthMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(AuthMiddleware);
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
