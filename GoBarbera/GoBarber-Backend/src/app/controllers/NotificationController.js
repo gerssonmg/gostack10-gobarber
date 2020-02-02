ee63a311c9fba81af39014edd04a4c80236ae21a
Gerson <gersoncafilho@gmail.com>
2020-02-02 15:43:20 -0300

    Add send email. Begin add template email

---
+++ GoBarbera/GoBarber-Backend/src/app/controllers/NotificationController.js
@@ -0,0 +1,37 @@
+import Notification from '../schemas/Notification';
+import User from '../models/User';
+
+class NotificaionController {
+  async index(req, res) {
+    const checkUserProvier = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true,
+      },
+    });
+
+    if (!checkUserProvier) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: 'desc' })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
+}
+
+export default new NotificaionController();
---
+++ GoBarbera/GoBarber-Backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'e0d255c34c6af5',
+    pass: 'e05d694bfa6fd6',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
