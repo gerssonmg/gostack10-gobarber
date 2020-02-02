a139be78d8294265308a36078b3bbe020aa39e8e
Gerson <gersoncafilho@gmail.com>
2020-02-02 08:45:10 -0300

    Begin Notification MongoDB

--- GoBarbera/GoBarber-Backend/src/routes.js
+++ GoBarbera/GoBarber-Backend/src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import AuthMiddleware from './app/middlewares/auth';
 
@@ -23,6 +24,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
