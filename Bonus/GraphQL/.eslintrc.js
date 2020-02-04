f70a428a303f5512f5e0164647aab359ff5f0a4d
Gerson <gersoncafilho@gmail.com>
2020-02-04 18:00:23 -0300

    Begin curser/bonus/GraphQL. Init apolo, config eslint/prettier

---
+++ Bonus/GraphQL/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "camelcase": "off"
+  },
+};
+
+//yarn eslint --init
+//yarn eslint --fix src --ext .js
+
+//yarn add prettier eslint-config-prettier eslint-plugin-prettier
