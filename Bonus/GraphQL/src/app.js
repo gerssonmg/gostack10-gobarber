490820ca0c619a7a743b6256d267cee2ff7655d4
Gerson <gersoncafilho@gmail.com>
2020-02-04 18:08:09 -0300

    Create .env :: code app.js

--- Bonus/GraphQL/src/app.js
+++ Bonus/GraphQL/src/app.js
@@ -1,29 +1,18 @@
 import 'dotenv/config';
 
-const a = [
-  1,
-  2,
-  3,
-  4,
-  53,
-  2,
-  2,
-  22,
-  3,
-  323,
-  2,
-  32,
-  2,
-  3,
-  23,
-  23,
-  2,
-  3,
-  23,
-  23,
-  23,
-  23,
-  23,
-  23,
-];
-console.log(a);
+import { ApolloServer } from 'apollo-server';
+import mongoose from 'mongoose';
+
+import schema from './schema';
+
+const server = new ApolloServer({
+  schema,
+  playground: process.env.NODE_ENV === 'development',
+});
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+export default server;
