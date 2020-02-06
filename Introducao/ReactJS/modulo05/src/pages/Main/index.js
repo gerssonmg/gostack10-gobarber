c9f7cc90038b6f172b791ece7658a2495d38cd80
Gerson <gersoncafilho@gmail.com>
2020-02-06 17:01:26 -0300

    Animation keyframe

--- Introducao/ReactJS/modulo05/src/pages/Main/index.js
+++ Introducao/ReactJS/modulo05/src/pages/Main/index.js
@@ -1,21 +1,49 @@
-import React from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
+import api from '../../services/api';
 
 export default function Main() {
+  const [newRepo, setNewrepo] = useState('rocketseat/unform');
+  const [resAxios, setResaxios] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  function handleInputChange(e) {
+    setNewrepo(e);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+
+    const response = await api.get(`/repos/${newRepo}`);
+    setResaxios([...resAxios, response.data.full_name]);
+    console.log(resAxios);
+    setLoading(false);
+  }
   return (
     <Container>
       <h1>
         <FaGithubAlt />
-        Repositorios
+        Repositorios{newRepo}
       </h1>
 
-      <Form onSubmit={() => { }}>
-        <input type="text" placeholder="Adicionar repositório" />
-        <SubmitButton disabled>
-          <FaPlus color="#FFF" size={14} />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório"
+          value={newRepo}
+          onChange={e => handleInputChange(e.target.value)}
+        />
+        <SubmitButton loading={loading}>
+          {loading ? (
+            <FaSpinner color="#FFF" size={14} />
+          ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
         </SubmitButton>
       </Form>
+      {resAxios}
     </Container>
   );
 }
