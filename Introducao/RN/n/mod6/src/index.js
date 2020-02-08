568fb0903e5edccae09503414aa6d69773384be2
Gerson <gersoncafilho@gmail.com>
2020-02-07 21:26:10 -0300

    react-native run-android pela 5 vez.criei o .editorconfig   apaguei o packager-lock.json e rodei yarn

--- Introducao/RN/n/mod6/src/index.js
+++ Introducao/RN/n/mod6/src/index.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import {
-  SafeAreaView,
+
   StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <>
-      <Text>OOOOOOOOO</Text>
+      <Text>55</Text>
     </>
   );
 }
