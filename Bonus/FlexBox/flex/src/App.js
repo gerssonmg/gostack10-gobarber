a2d8b7579002a91a7d42efd4d7ca53fb30d8d9bf
Gerson <gersoncafilho@gmail.com>
2020-02-10 13:56:27 -0300

    flex direction

---
+++ Bonus/FlexBox/flex/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <div id="app">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+      </div>
+    </body>
+  );
+}
+
+export default App;
