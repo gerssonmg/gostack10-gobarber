1c02ea5b1ad4849acbaebcd3ef932e096ccf7d21
Gerson <gersoncafilho@gmail.com>
2020-02-10 15:47:16 -0300

    header

--- Bonus/FlexBox/flex/src/App.js
+++ Bonus/FlexBox/flex/src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './App.css';
+import { FaUserCircle, FaTwitter } from 'react-icons/fa'
+import { IoIosNotificationsOutline, IoIosPerson } from 'react-icons/io'
+import { TiHomeOutline } from 'react-icons/ti'
+import { AiOutlineMail } from 'react-icons/ai'
 
 function App() {
   return (
     <body>
-      <div id="app">
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
-      </div>
-    </body>
+      <header id="main-header">
+        <div className="content">
+          <nav>
+            <ul>
+              <li><TiHomeOutline color="#000" />Home</li>
+              <li><IoIosNotificationsOutline color="#000" />Notifications</li>
+              <li><AiOutlineMail color="#000" />Messages</li>
+            </ul>
+          </nav>
+          <FaTwitter color="#3bb9e3"></FaTwitter>
+          <div className="side">
+            <i class="fa fa-youtube icon"></i>
+            <input type="text" placeholder="Search on Twitter" />
+            <IoIosPerson />
+            <button>Tweet</button>
+          </div>
+        </div>
+      </header>
+    </body >
   );
 }
 
