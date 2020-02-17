3f7da5460a5ee914264d81fbb06dcdd56509dff4
Gerson <gersoncafilho@gmail.com>
2020-02-16 21:42:27 -0300

    OK redux saga. Next features, intro navegation with saga

--- Introducao/ArquiteturaFlux/rocketshoes/src/store/modules/cart/sagas.js
+++ Introducao/ArquiteturaFlux/rocketshoes/src/store/modules/cart/sagas.js
@@ -1,19 +1,57 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
 
-  const data = {
-    ...response.data,
-    amount: 1,
-    priceFormatted: formatPrice(response.data.price),
-  };
+  const stock = yield call(api.get, `/stock/${id}`);
 
-  yield put(addToCartSuccess(data));
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitade fora de estoque');
+    return;
+  }
+
+  if (productExists) {
+    yield put(updateAmountSuccess(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  // const product = yield select(state => state.cart.find(p => p.id === id));
+
+  const stock = yield call(api.get, `stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitade fore de estoque');
+  }
+}
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
